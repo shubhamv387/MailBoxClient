00060a861225aba1f38f3e56ed848c1c97fbde95
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-12-02 14:49:35 +0530

    taliwind config changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-import withMT from '@material-tailwind/react/utils/withMT';
-
-export default withMT({
+export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
@@ -35,4 +33,4 @@ export default withMT({
     },
   },
   plugins: [],
-});
+};
